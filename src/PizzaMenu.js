348c913872e4223e00b1fd33c8e0ae3c7d70e367
harleigh <harleigh.marsh@gmail.com>
2023-09-13 09:03:22 -0700

    initial commit

---
+++ src/PizzaMenu.js
@@ -0,0 +1,9 @@
+
+export default function PizzaMenu() {
+    return(
+        <>
+        <h1>Pizza Time!!!</h1>
+        </>
+    )
+}
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './PizzaMenu';  //App is whatever function export default is 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

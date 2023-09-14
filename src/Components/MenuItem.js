b6e2796c292118dc633a8ff8c9ce4087e7ed33fa
harleigh <harleigh.marsh@gmail.com>
2023-09-14 14:53:26 -0700

    Added a sold our message if pizza is not in stock

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -6,6 +6,7 @@ export function MenuItem( {imgPath, name, description, price, isSoldOut}) {
         <li className={`pizza ${isSoldOut? "sold-out":""}`} >
             <img src={imgPath} alt={name} width={100} height={100}/>
                 <div>
+                    {isSoldOut? <span style={{color: "red", fontSize:"x-small"}} >Currently Sold Out!</span>:""}
                     <h3>{name}</h3>
                     <p>{description}</p>
                     <p>Price: {price}</p>
